f6bb26788fe052d6ee92fe078f66642a89a340de
Hassaan Raza <hassaan_raza1@yahoo.com>
2025-04-02 00:11:21 +0400

    revert

--- app/src/main/java/com/example/aiaproject/data/network/RequestInterceptor.kt
+++ app/src/main/java/com/example/aiaproject/data/network/RequestInterceptor.kt
@@ -7,7 +7,7 @@ class RequestHeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val modifiedRequest = originalRequest.newBuilder()
-            .header("Authorization", "Bearer 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")
+            .header("Authorization", "Bearer <YOUR API KEY>")
             .build()
         return chain.proceed(modifiedRequest)
     }
